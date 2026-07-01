86d2d53b18ba1bf8eeaecccb845b8641f2b501ef
cartikeya <cartikeya.official@gmail.com>
2026-07-01 14:31:20 +0530

    added jwt

--- server.js
+++ server.js
@@ -18,36 +18,44 @@ app.get("/", (req, res) => {
 });
 
 app.post("/register", async (req, res) => {
-  const { name, email, password } = req.body;
-  const existingUser = await User.findOne({ email });
-  if (existingUser) {
-    return res.status(409).send("Email already exists");
+  try {
+    const { name, email, password } = req.body;
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return res.status(409).send("Email already exists");
+    }
+    const hashedPassword = await bcrypt.hash(password, 10);
+    const user = new User({ name, email, password: hashedPassword });
+    await user.save();
+    console.log(user);
+    res.status(201).json({ message: "User registered successfully" });
+  } catch (error) {
+    res.status(500).send("Internal server error");
   }
-  const hashedPassword = await bcrypt.hash(password, 10);
-  const user = new User({ name, email, password: hashedPassword });
-  await user.save();
-  console.log(user);
-  res.send("User registered");
 });
 
 app.post("/login", async (req, res) => {
-  const { email, password } = req.body;
-  const user = await User.findOne({ email });
-  if (!user) {
-    return res.status(404).send("User not found");
-  }
-  const isMatch = await bcrypt.compare(password, user.password);
+  try {
+    const { email, password } = req.body;
+    const user = await User.findOne({ email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const isMatch = await bcrypt.compare(password, user.password);
 
-  if (!isMatch) {
-    return res.status(401).send("incorrect password");
+    if (!isMatch) {
+      return res.status(401).send("incorrect password");
+    }
+    const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+      expiresIn: "1d",
+    });
+    return res.json({
+      message: "Login successful",
+      token,
+    });
+  } catch (error) {
+    res.status(500).send("Internal server error");
   }
-  const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
-    expiresIn: "1d",
-  });
-  return res.json({
-    message: "Login successful",
-    token,
-  });
 });
 
 app.listen(PORT, () => console.log(`server running on ${PORT}`));
