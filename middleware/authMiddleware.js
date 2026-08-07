29cd9abc0f8486a38015585a275c3593b09dd176
cartikeya <cartikeya.official@gmail.com>
2026-08-07 09:11:14 +0530

    commiting changes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -4,10 +4,10 @@ const authMiddleware = (req, res, next) => {
   const authHeader = req.headers.authorization;
 
   if (!authHeader) {
-    return res.status(401).send("authheader not found");c
+    return res.status(401).send("authheader not found");
   }
-  const token = authHeader.split(" ")[1];
   try {
+    const token = authHeader.split(" ")[1];
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
     req.user = decoded;
     console.log(decoded);
